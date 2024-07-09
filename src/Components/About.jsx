82c80c0c34f9919d6bbce57e0ae82b0f3ab197f5
Karrar Esbeyajani <52514482+KEYAJANI@users.noreply.github.com>
2024-07-09 16:38:16 +0300

    UPDATE 0.2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,27 +15,27 @@ export default function About() {
           <li>
             <img
               className="aboutMePhotos burjBaghdad"
-              src="https://media.discordapp.net/attachments/827757330883018782/1192516673245294703/IMG_4841.jpg?ex=65a95cb2&is=6596e7b2&hm=eee15d7d27d6b5ff054615153cfb95e9cf28033c7f5926a9387bd71bb08151ae&=&format=webp&width=512&height=683"
+              src="https://i.ibb.co/N7HXWKh/IMG-4841.jpg"
               alt="burj-baghdad"
             />
           </li>
           <li>
             <img
               className="aboutMePhotos karrarKitty"
-              src="https://media.discordapp.net/attachments/827757330883018782/1192516673832505384/IMG_5144.jpg?ex=65a95cb2&is=6596e7b2&hm=5ae9f444fa1e1527dfa5d37228bd708a25c9eef90ff4b44e704eebffe53a1917&=&format=webp&width=512&height=683"
+              src="https://i.ibb.co/sQX5pGv/IMG-5144.jpg"
               alt="karrarKitty"
             />
           </li>
           <li>
             <img
               className="aboutMePhotos karrarPhoto"
-              src="https://media.discordapp.net/attachments/827757330883018782/1192516674906243172/IMG_0813.png?ex=65a95cb3&is=6596e7b3&hm=6ee21bc4914e9e66b434443ab612a0a4289a991c07113ae45033edde83693f81&=&format=webp&quality=lossless&width=391&height=683"
+              src="https://i.ibb.co/QC7fwJD/IMG-0813.jpg"
               alt="KarrarPhoto"
             />
           </li>
         </ul>
       </div>
-      <div  className="aboutForm">
+      <div className="aboutForm">
         <Grid className="aboutPaddingIssue" container spacing={2}>
           <Grid xs={3}>
             <h3
@@ -47,7 +47,7 @@ export default function About() {
           </Grid>
           <Grid className="aboutParagraph" xs={8}>
             <h3 style={{ fontWeight: "300", color: "black" }}>
-              Hello world, I'm Karrar Esbeyajani!
+              Hello World, I'm Karrar Esbeyajani!
             </h3>
             <h3
               style={{
@@ -57,12 +57,12 @@ export default function About() {
                 width: "540px",
               }}
             >
-              As a Junior Front-End Developer, I possess an impressive arsenal
-              of skills in <span style={{ fontWeight: "500" }}>HTML</span>,{" "}
-              <span style={{ fontWeight: "500" }}>CSS</span>,{" "}
+              As a Junior MERN Full-Stack Developer, I possess an impressive
+              arsenal of skills in{" "}
               <span style={{ fontWeight: "500" }}>JavaScript</span>,{" "}
-              <span style={{ fontWeight: "500" }}>React</span> and{" "}
-              <span style={{ fontWeight: "500" }}>Tailwind</span>. I excel in
+              <span style={{ fontWeight: "500" }}>React</span>,{" "}
+              <span style={{ fontWeight: "500" }}>NodeJS</span> and{" "}
+              <span style={{ fontWeight: "500" }}>MongoDB</span>. I excel in
               designing and maintaining responsive websites that offer a smooth
               user experience. My expertise lies in crafting dynamic, engaging
               interfaces through writing clean and optimized code and utilizing
